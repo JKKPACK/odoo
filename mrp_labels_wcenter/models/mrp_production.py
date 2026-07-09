2cc277e1d17085c1534b752b255cff4902285aa7
JKKPACK <adminodoo@jkkpack.com>
2026-07-08 22:32:48 -0600

    Update mrp_production.py

--- mrp_labels_wcenter/models/mrp_production.py
+++ mrp_labels_wcenter/models/mrp_production.py
@@ -5,12 +5,14 @@ class MrpProduction(models.Model):
 
     label_ids = fields.One2many('mrp.production.label', 'production_id', string="Etiquetas Emitidas")
 
-    def action_open_label_wizard(self):
+def action_open_label_wizard(self):
         self.ensure_one()
         # Sumamos los pesos de etiquetas ya emitidas para esta orden
         total_printed = sum(label.weight for label in self.label_ids)
-        # El límite restante debe ser el total de la orden menos lo ya pesado
-        weight_limit = self.product_qty - total_printed
+        
+        # Forzamos tomar la cantidad total de la orden (21.7070 de tu pantalla)
+        mo_total_qty = self.product_qty
+        weight_limit = mo_total_qty - total_printed
         
         # Encontrar el primer centro de trabajo de las operaciones para sugerirlo en el wizard
         default_wc = self.workorder_ids[0].workcenter_id.id if self.workorder_ids else False
@@ -23,7 +25,7 @@ class MrpProduction(models.Model):
             'target': 'new',
             'context': {
                 'default_production_id': self.id,
-                'default_mo_expected_weight': weight_limit if weight_limit > 0 else self.product_qty,
+                'default_mo_expected_weight': weight_limit if weight_limit > 0 else mo_total_qty,
                 'default_workcenter_id': default_wc,
             }
         }
