3ba0a80b5334b4f4a8b998bb3d6391af0eb9d96f
JKKPACK <adminodoo@jkkpack.com>
2026-08-04 10:00:11 -0600

    Delete mrp_packing_final/models/mrp_workcenter_1.py

--- mrp_packing_final/models/mrp_workcenter_1.py
+++
@@ -1,4 +0,0 @@
-from odoo import models, fields
-class MrpWorkcenter(models.Model):
-    _inherit = 'mrp.workcenter'
-    is_finished_product = fields.Boolean(string='Es Centro de Producto Terminado (Mamata/Embolsado)')
